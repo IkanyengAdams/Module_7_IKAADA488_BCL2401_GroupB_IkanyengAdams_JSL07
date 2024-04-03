1e2fe691fcae42fb7b74a2884befe5cf4b3ffb7d
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-04-03 15:48:34 +0200

    add the eventlistener and click in order for button to work

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   
     //  🚨 Close the modal when the close button is clicked
-    closeModal.("", function () {
+    closeModal.addEventListener("click", function () {
     modal.style.display = "none";  
     });
   });
